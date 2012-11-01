aa712e105f247ccf5f2cb7de812c47b5b7143722
UncleVasya (Oleg Ovharenko) <Oleg_Ovcharenko@list.ru>
2012-11-01 21:41:31 +0200

    Straight line detection.

--- gen/com/delin/speedlogger/R.java
+++ gen/com/delin/speedlogger/R.java
@@ -19,7 +19,7 @@ public final class R {
         public static final int button4=0x7f060005;
         public static final int buttonPreferences=0x7f060003;
         public static final int buttonStart=0x7f060002;
-        public static final int checkBox1=0x7f06000c;
+        public static final int cbStraightLine=0x7f06000c;
         public static final int ratingBar1=0x7f06000d;
         public static final int tableRow1=0x7f060006;
         public static final int tableRow3=0x7f060009;
--- src/com/delin/speedlogger/ResultsActivity.java
+++ src/com/delin/speedlogger/ResultsActivity.java
@@ -5,12 +5,13 @@ import java.util.List;
 import android.app.Activity;
 import android.location.Location;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class ResultsActivity extends Activity {
 	TextView maxSpeedText;
 	TextView distanceText;
+	CheckBox cbStraightLine;
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -23,6 +24,38 @@ public class ResultsActivity extends Activity {
 		double x1 = loc1.getLatitude(); double y1 = loc1.getLongitude();
 		double x2 = loc2.getLatitude(); double y2 = loc2.getLongitude();
 		return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+	}
+	
+	public boolean StraightLine3D(List<Location> locList) {
+		if (locList.size() <= 2)
+			return true;
+		else {
+			double x1 = locList.get(0).getLatitude();
+			double y1 = locList.get(0).getLongitude();
+			double z1 = locList.get(0).getAltitude();
+			double x2 = locList.get(1).getLatitude();
+			double y2 = locList.get(1).getLongitude();
+			double z2 = locList.get(1).getAltitude();
+			
+			double dx = x2 - x1;
+			double dy = y2 - y1;
+			double dz = z2 - z1;
+			
+			// TODO: I have no idea what eps should be
+			double eps = 0.001;
+			for (int i=2; i<locList.size(); i++){
+				double x = locList.get(i).getLatitude();
+				double y = locList.get(i).getLongitude();
+				double z = locList.get(i).getAltitude();
+				
+				if  ((x1-x)/dx - (y1-y)/dy >= eps 
+					|| (x1-x)/dx - (z1-z)/dz >= eps)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
 	}
 	
 	public void onSessionFinished(List<Location> mLocList){
@@ -41,6 +74,8 @@ public class ResultsActivity extends Activity {
 		Location dest = mLocList.get(mLocList.size());
 		double distance = Distance(origin, dest);
 		distanceText.setText(Double.toString(distance));
+		
+		cbStraightLine.setChecked(StraightLine3D(mLocList));
 	}
 
 }
