b68f5167e120394d05ae0c2d2cbca111c8483f38
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2013-02-28 20:46:06 +0200

    distPointToLine3D

--- src/com/delin/speedlogger/ResultsActivity.java
+++ src/com/delin/speedlogger/ResultsActivity.java
@@ -47,40 +47,68 @@ public class ResultsActivity extends Activity {
         return y;
     }
 	
-	// TODO: it must be deleted or rewritten
-	public boolean StraightLine3D(List<Location> locList) {
-		if (locList.size() <= 2)
-			return true;
-		else {
-			double x1 = locList.get(0).getLatitude();
-			double y1 = locList.get(0).getLongitude();
-			double z1 = locList.get(0).getAltitude();
-			double x2 = locList.get(1).getLatitude();
-			double y2 = locList.get(1).getLongitude();
-			double z2 = locList.get(1).getAltitude();
-			
-			double dx = x2 - x1;
-			double dy = y2 - y1;
-			double dz = z2 - z1;
-			
-			// TODO: I have no idea what eps should be
-			double eps = 0.001;
-			for (int i=2; i<locList.size(); i++){
-				double x = locList.get(i).getLatitude();
-				double y = locList.get(i).getLongitude();
-				double z = locList.get(i).getAltitude();
-				
-				if  ((x1-x)/dx - (y1-y)/dy >= eps 
-					|| (x1-x)/dx - (z1-z)/dz >= eps)
-				{
-					return false;
-				}
-			}
-			return true;
-		}
+	double sqrDistPointToPoint3D(double ax, double ay, double az, double bx, double by, double bz){
+	    return (ax-bx)*(ax-bx) + (ay-by)*(ay-by) + (az-bz)*(az-bz);
 	}
 	
-	public boolean StraightLine2D(List<Location> locList) {
+	// got it here: http://krasprog.ru/persons.php?page=demidenko&blog=10
+	double distPointToLine(double Px, double Py, double Pz, double Ax, double Ay, double Az, 
+						   double Bx, double By, double Bz)
+	{
+	    double pa, pb, ab; // squares of distances PA, PB, AB
+	    pa = sqrDistPointToPoint3D(Px,Py,Pz,Ax,Ay,Az);
+	    pb = sqrDistPointToPoint3D(Px,Py,Pz,Bx,By,Bz);
+	    ab = sqrDistPointToPoint3D(Ax,Ay,Az,Bx,By,Bz);
+	    
+	    // if perpendicular is out of line then closest point is one of line edges
+	    if(pa >= pb + ab) return Math.sqrt(pb);
+	    if(pb >= pa + ab) return Math.sqrt(pa);
+	    
+	    double a1, a2, a3, b1, b2, b3; // temp vars
+	    a1=Ax-Px; a2=Ay-Py; a3=Az-Pz;
+	    b1=Bx-Px; b2=By-Py; b3=Bz-Pz;
+	    
+	    return Math.sqrt(sqrDistPointToPoint3D(a2*b3-a3*b2,-(a1*b3-a3*b1),a1*b2-b1*a2,0,0,0)/ab);
+	}
+	
+	// TODO: it must be deleted or rewritten
+	public boolean StraightLine3D(List<Location> locList){
+		// for a small path we assume it is a straight line
+		// TODO: 2 better be replaced with something
+		if (locList.size() <= 2) return true;
+		
+		Location origin = locList.get(0);
+		Location dest = locList.get(locList.size()-1);
+			
+		// lat/lon to UTM convert is necessary to perform line calculus
+		// UTM values represented in meters
+		// altitude's already in meters, no conversion needed
+		double x1 = mercX(origin.getLongitude());
+		double y1 = mercY(origin.getLatitude());
+		double z1 = origin.getAltitude();
+		double x2 = mercX(dest.getLongitude());
+		double y2 = mercY(dest.getLatitude());
+		double z2 = dest.getAltitude();
+		
+		// TODO: get proper number for eps
+		// TODO: move eps to the place where constants live
+		double eps = 5.d; // max allowed deviation from the line (meters)
+		for (int i=2; i<locList.size(); i++){
+			Location loc = locList.get(i);
+			double x = mercX(loc.getLongitude());
+			double y = mercY(loc.getLatitude());
+			double z = loc.getAltitude();
+			double d = distPointToLine(x,y,z,x1,y1,z1,x2,y2,z2);
+			if (d > eps) return false;
+		}
+		return true;
+	}
+	
+	public boolean StraightLine2D(List<Location> locList){
+		// for a small path we assume it is a straight line
+		// TODO: 2 better be replaced with something
+		if (locList.size() <= 2) return true;
+		
 		Location origin = locList.get(0);
 		Location dest = locList.get(locList.size()-1);
 		
@@ -91,28 +119,22 @@ public class ResultsActivity extends Activity {
 		double x2 = mercX(dest.getLongitude());
 		double y2 = mercY(dest.getLatitude());
 		
-		// TODO: 2 better be replaced with something
-		if (locList.size() <= 2 || Distance(x1, y1, x2, y2) < 50)
-			return true; // for such a small path we assume it is a straight line
-		else {	
-			// line formula Ax + By + C = 0
-			double A = y1 - y2;
-		    double B = x2 - x1;
-		    double C = x1*y2 - x2*y1;
+		// line formula Ax + By + C = 0
+		double A = y1 - y2;
+		double B = x2 - x1;
+		double C = x1*y2 - x2*y1;
 			
-		    // TODO: get proper number for eps
-		    // TODO: move eps to the place where constants live
-			double eps = 5.d; // max allowed deviation from the line (meters)
-			for (int i=2; i<locList.size(); i++){
-				Location loc = locList.get(i);
-				double x = mercX(loc.getLongitude());
-				double y = mercY(loc.getLatitude());
-				double d = (A*x+B*y+C)/Math.sqrt(A*A+B*B); // distance from point to line
-				if (d > eps)
-					return false;
-			}
-			return true;
+		// TODO: get proper number for eps
+		// TODO: move eps to the place where constants live
+		double eps = 5.d; // max allowed deviation from the line (meters)
+		for (int i=2; i<locList.size(); i++){
+			Location loc = locList.get(i);
+			double x = mercX(loc.getLongitude());
+			double y = mercY(loc.getLatitude());
+			double d = (A*x+B*y+C)/Math.sqrt(A*A+B*B); // distance from point to line
+			if (d > eps) return false;
 		}
+		return true;
 	}
 	
 	public void onSessionFinished(List<Location> mLocList){
@@ -129,8 +151,7 @@ public class ResultsActivity extends Activity {
 		// printing distance
 		Location origin = mLocList.get(0);
 		Location dest = mLocList.get(mLocList.size());
-		double distance = Distance(origin.getLatitude(), origin.getLongitude(), 
-				                   dest.getLatitude(), dest.getLongitude());
+		double distance = origin.distanceTo(dest);
 		mDistance.setText(Double.toString(distance));
 		
 		mStraightLine.setChecked(StraightLine2D(mLocList));
