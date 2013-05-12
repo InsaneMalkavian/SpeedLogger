8703432194cdf2fbd3cd0663d7c19c8818a6575c
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2013-05-12 16:50:25 +0300

    Select GPX-file to read by FileGPS.

--- src/com/delin/speedlogger/GPS/FileGPSProvider.java
+++ src/com/delin/speedlogger/GPS/FileGPSProvider.java
@@ -1,20 +1,29 @@
 package com.delin.speedlogger.GPS;
 
+import com.delin.speedlogger.R;
 import com.delin.speedlogger.Serialization.GPXSerializer;
 import com.delin.speedlogger.Utils.Timer;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 
 public class FileGPSProvider extends GPSProvider {
+	static final String GPS_DIR_NAME = "FileGPS";
+	static final String DEFAULT_GPX = GPS_DIR_NAME + "/" + "default.gpx";
 	
 	GPXSerializer mGPXReader = null;
 	Timer mTimer = null;
 
 	public FileGPSProvider(Context context, LocationListener listener) {
 		super(context, listener);
-		mGPXReader = new GPXSerializer("gps2read.gpx", false);
+		// get gpx filename from devPrefs
+		String devPrefsName = context.getString(R.string.DevPrefs);
+		SharedPreferences devPrefs = context.getSharedPreferences(devPrefsName,Context.MODE_PRIVATE);
+		String gpxFilename = devPrefs.getString(context.getString(R.string.FileWithGPS),DEFAULT_GPX);
+		// load gpx & launch provider
+		mGPXReader = new GPXSerializer(gpxFilename, false);
 		mTimer = new Timer(new Runnable() {
 			@Override
 		     public void run() {
