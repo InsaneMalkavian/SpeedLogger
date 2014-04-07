fd626b048a0cbf65574c089f90a1946771615187
Wilde <gothdelin@gmail.com>
2014-04-08 01:03:39 +0400

    ResultsView Distances table created.

--- src/com/delin/speedlogger/Activities/ResultsActivity.java
+++ src/com/delin/speedlogger/Activities/ResultsActivity.java
@@ -46,7 +46,9 @@ public class ResultsActivity extends Activity {
     private TextView mMaxSpeed;
     private TextView mDistance;
     private TextView mTime;
-    private final static float speedDiff = 20.f;
+    private final static float SPEED_DIFF = 20.f;
+    private final static float DISTANCES[] = {201.f, 402.f, 804.f, 1000.f, 1609.f};
+    private final static String DISTANCES_STR[] = {"1/8 mile:", "1/4 mile:", "1/2 mile:", "1000m:", "1 mile:"};
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -73,8 +75,9 @@ public class ResultsActivity extends Activity {
         boolean isDownhillOK = result.getDownhillOK();
 
         float maxSpeed = result.GetMaxSpeed();
+        float totalRunDist = result.GetTotalDistance();
         mMaxSpeed.setText(String.format("%.3f", Converter.ms2kph(maxSpeed)) + " km/h");
-        mDistance.setText(String.format("%.3f", result.GetTotalDistance()) + " meters");
+        mDistance.setText(String.format("%.3f", totalRunDist) + " meters");
         mTime.setText(String.format("%.3f", (float)result.GetTotalTime()/1000) + " sec");
         ((CheckBox)findViewById(R.id.cbStraightLine)).setChecked(isStraightLine);
         ((CheckBox)findViewById(R.id.cbUphill)).setChecked(isDownhillOK);
@@ -83,50 +86,19 @@ public class ResultsActivity extends Activity {
             ResultsManager.getInstance().addResult(result);
         }
 
-        /*Log.i("Results Activity","Locs in path: " + Integer.toString(locList.size()));
-        for(int i=0; i<locList.size(); ++i){
-            Log.i("Results Activity", "-----\n" + "#" + Integer.toString(i) +
-                  "\n" + Logger.LocToStr(locList.get(i)));
-        }
-        // TODO: make use of interpolation; now it only shows values
-        Interpolator interp = new Interpolator(locList);
-        Location loc;
-        for(long i = locList.get(0).getTime(); i <= locList.get(locList.size()-1).getTime(); i += 250){
-            loc = interp.SpeedByTime(i);
-            Log.i("Results Activity", "-----\n" + "time: " + Long.toString(i) +
-                      "  speed: " + Double.toString(Converter.ms2kph(loc.getSpeed())));
-        }
-        for(float i = locList.get(0).getSpeed(); i <= locList.get(locList.size()-1).getSpeed(); i += 0.5){
-            loc = interp.TimeBySpeed(i);
-            Log.i("Results Activity", "-----\n" + "speed: " + Double.toString(Converter.ms2kph(i)) +
-                      "  time: " + Long.toString(loc.getTime()));
-        }
-
-        Location atSpeed = interp.TimeBySpeed(Converter.kph2ms(maxSpeed));
-        mTime.setText(Float.toString((float)(atSpeed.getTime()-locList.get(0).getTime())/1000)+" sec");
-        */
-
         TableLayout table = (TableLayout)findViewById(R.id.TableLayout01);
-
-        for (float speed = speedDiff; speed<=Converter.ms2kph(maxSpeed); speed+=speedDiff) {
+        for (float speed = SPEED_DIFF; speed<=Converter.ms2kph(maxSpeed); speed+=SPEED_DIFF) {
             TableWrapper tableWrapperRow = new TableWrapper(this, "0 - " + Integer.toString((int)speed)+":",
                     String.format("%.3f", (float)result.GetTimeAtSpeed(Converter.kph2ms(speed))/1000) + " sec");
             table.addView(tableWrapperRow.row);
         }
 
-        /*Button button = new Button(this);
-        button.setText("One more Button");
-        button.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
-        LinearLayout buttons = (LinearLayout)findViewById(R.id.LinearLayout01);
-        buttons.addView(button);*/
-/*        TextView mLeftText = new TextView(this);
-        mLeftText.setText("Left");
-        mLeftText.setGravity(Gravity.RIGHT);
-        mLeftText.setTextAppearance(this, android.R.attr.textAppearanceSmall);
-        mLeftText.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
-
-        buttons.addView(mLeftText);
-*/
+        table = (TableLayout)findViewById(R.id.TableLayoutDistances);
+        for (int i = 0; i<DISTANCES.length && DISTANCES[i]<=totalRunDist; i++) {
+            TableWrapper tableWrapperRow = new TableWrapper(this, DISTANCES_STR[i],
+                    String.format("%.3f", (float)result.GetTimeAtDistance(DISTANCES[i])/1000) + " sec");
+            table.addView(tableWrapperRow.row);
+        }
     }
 
     void ShowZeroResults() {
